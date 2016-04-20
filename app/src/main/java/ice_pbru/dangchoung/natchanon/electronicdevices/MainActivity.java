8da8e51b3c10601392812711e3ee95e4bcdb542a
Natchanon_Dream <natchanon38.dc@gmail.com>
2016-04-20 16:02:08 +0700

    ทำ aboutme เสร็จ กำลังจะทำ listview ต่อ

--- app/src/main/java/ice_pbru/dangchoung/natchanon/electronicdevices/MainActivity.java
+++ app/src/main/java/ice_pbru/dangchoung/natchanon/electronicdevices/MainActivity.java
@@ -1,13 +1,51 @@
 package ice_pbru.dangchoung.natchanon.electronicdevices;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Expercit
+    private Button aboutmeButton;
+    private ListView electroniclistview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        buttonController();
+
+        createListView();
     }
-}
+
+    private void createListView() {
+    }
+
+    private void buttonController() {
+        aboutmeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.youtube.com"));
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void bindWidget() {
+        aboutmeButton = (Button) findViewById(R.id.button);
+        electroniclistview = (ListView) findViewById(R.id.listView);
+
+    }//Bind Widget
+}//Main Class
